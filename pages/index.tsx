6b2543b518fad5be2625554bb2ef7f78255c2ec5
Nicholas Bottone <nick@bottone.io>
2022-05-02 17:25:04 -0400

    style: Format and remove .idea

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,38 @@
-import type {NextPage} from "next";
+import type { NextPage } from "next";
 import Link from "next/link";
-import {Button, Container, Image} from "react-bootstrap";
+import { Button, Container, Image } from "react-bootstrap";
 import React from "react";
 
 const Home: NextPage = () => {
-    return (
-        <Container className="text-center">
-            <Image
-                src="/blueno.png"
-                alt="Blueno logo"
-                width="300"
-                height="300"
-                className="d-inline-block align-top"
-            />
-            <h1>Wordify</h1>
-            <Link href="/game">
-                <Button variant="light" size="lg">
-                    Play As Guest
-                </Button>
-            </Link>
-            <br/>
-            <Link href="/#">
-                <Button variant="light" size="lg">
-                    Log In
-                </Button>
-            </Link>
-            <br/>
-            <Link href="/profile">
-                <Button variant="light" size="lg">
-                    Profile
-                </Button>
-            </Link>
-        </Container>
-    );
+  return (
+    <Container className="text-center">
+      <Image
+        src="/blueno.png"
+        alt="Blueno logo"
+        width="300"
+        height="300"
+        className="d-inline-block align-top"
+      />
+      <h1>Wordify</h1>
+      <Link href="/game">
+        <Button variant="light" size="lg">
+          Play As Guest
+        </Button>
+      </Link>
+      <br />
+      <Link href="/#">
+        <Button variant="light" size="lg">
+          Log In
+        </Button>
+      </Link>
+      <br />
+      <Link href="/profile">
+        <Button variant="light" size="lg">
+          Profile
+        </Button>
+      </Link>
+    </Container>
+  );
 };
 
 export default Home;

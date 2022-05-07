05992e7726f1b0b875e7044d2782ca90ff4bc712
Nicholas Bottone <nick@bottone.io>
2022-05-07 19:51:16 -0400

    fix: Make callback url generic

--- lib/auth/passport.ts
+++ lib/auth/passport.ts
@@ -25,7 +25,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || "",
+      callbackURL: "/api/auth/callback",
     },
     async (_accessToken, _refreshToken, profile, done) => {
       // Attempt to find user in database

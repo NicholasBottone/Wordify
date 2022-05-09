1c12d926d49d9f48e59b9d4cc9b956d14a3b1c51
Nicholas Bottone <nick@bottone.io>
2022-05-09 16:27:24 -0400

    feat: Implement nightly cron job

---
+++ pages/api/cron/midnight.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "../../../lib/endpoints/express";
+import nc from "next-connect";
+import mongoose from "../../../lib/middlewares/mongoose";
+import DailyPuzzle from "../../../models/DailyPuzzle";
+import User from "../../../models/User";
+
+const handler = nc<Request, Response>();
+
+handler.use(mongoose);
+
+handler.post(async (req, res) => {
+  // Check for the secret API key
+  if (req.headers["x-api-key"] !== process.env.API_KEY) {
+    return res.status(401).send("Invalid API key");
+  }
+
+  // Increment daysAgo on all daily puzzles
+  DailyPuzzle.updateMany(
+    {},
+    {
+      $inc: {
+        daysAgo: 1,
+      },
+    }
+  );
+
+  // Insert null at index 0 into pastGuesses and pastTimes for all users
+  User.updateMany(
+    {},
+    {
+      $push: {
+        pastGuesses: {
+          $each: [null],
+          $position: 0,
+        },
+        pastTimes: {
+          $each: [null],
+          $position: 0,
+        },
+      },
+    }
+  );
+
+  res.status(200).send("OK");
+});

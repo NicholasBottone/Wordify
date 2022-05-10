a5f2ea5ead09b11fba36afbd93650d2b2d1c191f
christinej604 <77814489+christinej604@users.noreply.github.com>
2022-05-10 04:01:16 -0400

    feat: Log in button works on homescreen

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import { Button, Container, Image } from "react-bootstrap";
 import React from "react";
+import { login, useUser } from "../lib/hooks/auth";
 
 const Home: NextPage = () => {
+  const { user } = useUser();
   return (
     <Container className="text-center">
       <Image
@@ -20,11 +22,10 @@ const Home: NextPage = () => {
         </Button>
       </Link>
       <br />
-      <Link href="/#">
-        <Button variant="light" size="lg">
-          Log In
-        </Button>
-      </Link>
+      <Button variant="light" size="lg" onClick={login}>
+        {" "}
+        Log In{" "}
+      </Button>
     </Container>
   );
 };

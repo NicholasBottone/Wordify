65166e2130c9cc9997481ab87b7203d2b5b8460c
Toshiki Kato <toshikikato@toshikis-air.devices.brown.edu>
2022-05-12 18:42:08 -0400

    feat: added bio and avg time in statistics

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,61 +3,76 @@ import { Button, Container, ProgressBar, Image } from "react-bootstrap";
 import Link from "next/link";
 import { FaExpand } from "react-icons/fa";
 import { useUser } from "../lib/hooks/auth";
+import { login } from "../lib/hooks/auth";
 
 export default function Profile() {
   const { user } = useUser();
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6 text-center">
-          <br />
-          <br />
-          <br />
-          <br />
-          <Image
-            src={user?.profilePicture}
-            alt="Profile picture"
-            width="200"
-            height="200"
-            roundedCircle
-          />
-          <br />
-          <br />
-          <h4>
-            {user?.givenName} {user?.familyName}
-          </h4>
-          <br />
-          <br />
-          <Link href="/game">
-            <Button variant="secondary" size="lg" className="">
-              Real Word of the Day <FaExpand />
-            </Button>
-          </Link>{" "}
-        </div>
-        <div className="col-sm-6">
-          <Container className="mt-5">
-            <Link href="/profile">
-              <Button variant="secondary" size="sm" className="">
-                Dashboard
+  if (user) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 text-center">
+            <br />
+            <br />
+            <br />
+            <br />
+            <Image
+              src={user?.profilePicture}
+              alt="Profile picture"
+              width="200"
+              height="200"
+              roundedCircle
+            />
+            <br />
+            <br />
+            <h4>
+              {user?.givenName} {user?.familyName}
+            </h4>
+            {user?.bio}]
+            <br />
+            <br />
+            <Link href="/game">
+              <Button variant="secondary" size="lg" className="">
+                Real Word of the Day <FaExpand />
               </Button>
             </Link>{" "}
-            <Link href="/statistics">
-              <Button variant="light" size="sm">
-                Statistics
-              </Button>
-            </Link>
-          </Container>
-          <Container className="mt-5 d-grid gap-2"></Container>
-          <h1> Board </h1>
-          <h1 className="text-centered">
-            ⬜⬜⬜⬜⬜ <br />
-            ⬜⬜⬜⬜⬜ <br />
-            ⬜⬜⬜⬜⬜ <br />
-            ⬜⬜⬜⬜⬜ <br />
-            ⬜⬜⬜⬜⬜ <br />
-          </h1>
+          </div>
+          <div className="col-sm-6">
+            <Container className="mt-5">
+              <Link href="/profile">
+                <Button variant="secondary" size="sm" className="">
+                  Dashboard
+                </Button>
+              </Link>{" "}
+              <Link href="/statistics">
+                <Button variant="light" size="sm">
+                  Statistics
+                </Button>
+              </Link>
+            </Container>
+            <Container className="mt-5 d-grid gap-2"></Container>
+            <h1> Board </h1>
+            <h1 className="text-centered">
+              ⬜⬜⬜⬜⬜ <br />
+              ⬜⬜⬜⬜⬜ <br />
+              ⬜⬜⬜⬜⬜ <br />
+              ⬜⬜⬜⬜⬜ <br />
+              ⬜⬜⬜⬜⬜ <br />
+            </h1>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } 
+  return (
+    <Container className="text-center">
+      <br />
+      <h4> Log in to access profile page!</h4>
+      <br />
+    <Button variant="light" size="lg" onClick={login}>
+    {" "}
+    Log In{" "}
+  </Button>
+  </Container>
+  )
 }
--- pages/statistics.tsx
+++ pages/statistics.tsx
@@ -3,6 +3,7 @@ import { Button, Container, ProgressBar, Image } from "react-bootstrap";
 import Link from "next/link";
 import { useUser } from "../lib/hooks/auth";
 import { FaExpand } from "react-icons/fa";
+import { login } from "../lib/hooks/auth";
 
 export default function Profile() {
   const { user, isLoading } = useUser();
@@ -22,6 +23,7 @@ export default function Profile() {
   let { sixGuesses }: any = 0;
   let { maxGuesses }: any = 0;
   let { times }: any = 0;
+  let { averageTime }: any = 0;
   if (user) {
     gamesWon = user?.gamesWon;
     gamesPlayed = user?.gamesPlayed;
@@ -36,112 +38,128 @@ export default function Profile() {
     fiveGuesses = guessDistribution[4];
     sixGuesses = guessDistribution[5];
     times = user?.pastTimes;
-    console.log(times);
+    averageTime = times.length;
+    console.log("times is" + times);
     maxGuesses = Math.max.apply(null, guessDistribution);
     console.log(maxGuesses);
   }
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-6 text-center">
-          <br />
-          <br />
-          <br />
-          <br />
-          <Image
-            src={user?.profilePicture}
-            alt="Profile picture"
-            width="200"
-            height="200"
-            className="d-inline-block"
-            roundedCircle
-          />
-          <br />
-          <br />
-          <h4>
-            {user?.givenName} {user?.familyName}
-          </h4>
-          <br />
-          <br />
-          <Link href="/game">
-            <Button variant="secondary" size="lg" className="">
-              Real Word of the Day <FaExpand />
-            </Button>
-          </Link>{" "}
-        </div>
-        <div className="col-sm-6">
-          <Container className="mt-5">
-            <Link href="/profile">
-              <Button variant="light" size="sm" className="">
-                Dashboard
+  if (user) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 text-center">
+            <br />
+            <br />
+            <br />
+            <br />
+            <Image
+              src={user?.profilePicture}
+              alt="Profile picture"
+              width="200"
+              height="200"
+              className="d-inline-block"
+              roundedCircle
+            />
+            <br />
+            <br />
+            <h4>
+              {user?.givenName} {user?.familyName}
+            </h4>
+            {user?.bio}]
+            <br />
+            <br />
+            <Link href="/game">
+              <Button variant="secondary" size="lg" className="">
+                Real Word of the Day <FaExpand />
               </Button>
             </Link>{" "}
-            <Link href="/statistics">
-              <Button variant="secondary" size="sm">
-                Statistics
-              </Button>
-            </Link>
-          </Container>
-          <Container className="mt-5 d-grid gap-2">
-            <h1>Statistics </h1>
-            <table className="Statistics">
-              <thead>
-                <tr id="statisticRow">
-                  <th scope="col"> {gamesPlayed}</th>
-                  <th scope="col">
-                    {" "}
-                    {winPercentage} {`%`}{" "}
-                  </th>
-                  <th scope="col"> {winStreak} </th>
-                  <th scope="col"> {maxStreak} </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr id="statisticRow">
-                  <td scope="row">Played</td>
-                  <td>Win %</td>
-                  <td>Current Streak</td>
-                  <td>Maximum Streak</td>
-                </tr>
-              </tbody>
-            </table>
-          </Container>
-          <Container className="mt-5 d-grid gap-2">
-            <h1> Guess Distribution </h1>
-            <ProgressBar
-              now={oneGuess == 0 ? 0 : 100 / (maxGuesses / oneGuess)}
-              label={oneGuess}
-              variant="success"
-            />
-            <ProgressBar
-              now={twoGuesses == 0 ? 0 : 100 / (maxGuesses / twoGuesses)}
-              label={twoGuesses}
-              variant="secondary"
-            />
-            <ProgressBar
-              now={threeGuesses == 0 ? 0 : 100 / (maxGuesses / threeGuesses)}
-              label={threeGuesses}
-              variant="secondary"
-            />
-            <ProgressBar
-              now={fourGuesses == 0 ? 0 : 100 / (maxGuesses / fourGuesses)}
-              label={fourGuesses}
-              variant="secondary"
-            />
-            <ProgressBar
-              now={fiveGuesses == 0 ? 0 : 100 / (maxGuesses / fiveGuesses)}
-              label={fiveGuesses}
-              variant="secondary"
-            />
-            <ProgressBar
-              now={sixGuesses == 0 ? 0 : 100 / (maxGuesses / sixGuesses)}
-              label={sixGuesses}
-              variant="secondary"
-            />
-          </Container>
+          </div>
+          <div className="col-sm-6">
+            <Container className="mt-5">
+              <Link href="/profile">
+                <Button variant="light" size="sm" className="">
+                  Dashboard
+                </Button>
+              </Link>{" "}
+              <Link href="/statistics">
+                <Button variant="secondary" size="sm">
+                  Statistics
+                </Button>
+              </Link>
+            </Container>
+            <Container className="mt-5 d-grid gap-2">
+              <h1>Statistics </h1>
+              <table className="Statistics">
+                <thead>
+                  <tr id="statisticRow">
+                    <th scope="col"> {gamesPlayed}</th>
+                    <th scope="col">
+                      {" "}
+                      {winPercentage} {`%`}{" "}
+                    </th>
+                    <th scope="col"> {winStreak} </th>
+                    <th scope="col"> {maxStreak} </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr id="statisticRow">
+                    <td scope="row">Played</td>
+                    <td>Win %</td>
+                    <td>Current Streak</td>
+                    <td>Maximum Streak</td>
+                    <td>Average Time</td>
+                  </tr>
+                </tbody>
+              </table>
+            </Container>
+            <Container className="mt-5 d-grid gap-2">
+              <h1> Guess Distribution </h1>
+              <ProgressBar
+                now={oneGuess == 0 ? 0 : 100 / (maxGuesses / oneGuess)}
+                label={oneGuess}
+                variant="success"
+              />
+              <ProgressBar
+                now={twoGuesses == 0 ? 0 : 100 / (maxGuesses / twoGuesses)}
+                label={twoGuesses}
+                variant="secondary"
+              />
+              <ProgressBar
+                now={threeGuesses == 0 ? 0 : 100 / (maxGuesses / threeGuesses)}
+                label={threeGuesses}
+                variant="secondary"
+              />
+              <ProgressBar
+                now={fourGuesses == 0 ? 0 : 100 / (maxGuesses / fourGuesses)}
+                label={fourGuesses}
+                variant="secondary"
+              />
+              <ProgressBar
+                now={fiveGuesses == 0 ? 0 : 100 / (maxGuesses / fiveGuesses)}
+                label={fiveGuesses}
+                variant="secondary"
+              />
+              <ProgressBar
+                now={sixGuesses == 0 ? 0 : 100 / (maxGuesses / sixGuesses)}
+                label={sixGuesses}
+                variant="secondary"
+              />
+            </Container>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return (
+    <Container className="text-center">
+      <br />
+      <h4> Log in to access the statistics page!</h4>
+      <br />
+    <Button variant="light" size="lg" onClick={login}>
+    {" "}
+    Log In{" "}
+  </Button>
+  </Container>
+  )
 }

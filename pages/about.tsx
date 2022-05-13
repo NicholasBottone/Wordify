af2b6cbf50f497c6959d31b22fc050cce7e7a68c
Nicholas Bottone <nick@bottone.io>
2022-05-13 19:23:04 -0400

    feat: add about page to navbar

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,17 +3,25 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h1 className= "text-centered">Welcome to Wordify! This is the about page.</h1>
+      <h1 className="text-centered">
+        Welcome to Wordify! This is the about page.
+      </h1>
       <br />
-      <p>Hi! We are Nick, Robert, James, Christine, Andrew, and Toshi! We are students at Brown Unversity 
-        who made this project as our final for CSCI0320: Introduction to Software Engineering. We are a group 
-        who loves word games and wanted to make something that us and our fellow word game lovers could enjoy.
+      <p>
+        Hi! We are Nick, Robert, James, Christine, Andrew, and Toshi! We are
+        students at Brown University who made this project as our final for
+        CSCI0320: Introduction to Software Engineering. We are a group who loves
+        word games and wanted to make something that us and our fellow word game
+        lovers could enjoy.
       </p>
       <br />
-      <p>This is a spinoff of one of the word games that have been trending lately. However, we wanted our users
-        to be more connected. Thus, we also included a feature where you can see the results of all people who
-        played the puzzle for the day! We also added a profile page where you can see a more detailed analysis 
-        of your gameplay statistics. We hope you enjoy!
+      <p>
+        This is a spinoff of one of the word games that have been trending
+        lately. However, we wanted our users to be more connected. Thus, we also
+        included a feature where you can see the results of all people who
+        played the puzzle for the day! We also added a profile page where you
+        can see a more detailed analysis of your game play statistics. We hope
+        you enjoy!
       </p>
     </div>
   );

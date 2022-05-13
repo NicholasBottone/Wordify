5af8a086c4ea06f36b475d3259b54b4f40f27ceb
Nicholas Bottone <nick@bottone.io>
2022-05-13 05:56:45 -0400

    fix: Check if query string exists on search endpoint

--- pages/api/user/search.ts
+++ pages/api/user/search.ts
@@ -4,23 +4,20 @@ import mongoose from "../../../lib/middlewares/mongoose";
 import auth from "../../../lib/middlewares/auth";
 import { isLoggedIn } from "../../../lib/middlewares/user";
 import User from "../../../models/User";
-import IUser from "../../../types/IUser";
-import { send } from "process";
 
 const handler = nc<Request, Response>();
 
 handler.use(mongoose, ...auth, isLoggedIn);
 
 /**
- * find all of the users that are a partial name match for the given query
- * string
+ * Find up to 10 users that are a partial name match for the given query string
  */
 handler.get(async (req, res) => {
   // get the query string
-  const query = req.query.q as String;
+  const query = req.query.q;
 
-  if (query.length < 2) {
-    res
+  if (!query || query.length < 2) {
+    return res
       .status(400)
       .send("Query too short! Please provide a query of length at least 2.");
   }
